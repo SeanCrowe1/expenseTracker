388af3c05e8885ddb6e27f9d06d69febeaa6dd74
SeanCrowe1 <sean.crowe2002@gmail.com>
2026-07-01 12:39:29 +0100

    implemented basic interface add functionality (fix clear + error handling)

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ class ExpenseTrackerApp:
         self.name_var = tk.StringVar()
         self.amount_var = tk.StringVar()
         self.date_var = tk.StringVar()
-        self.type_var = tk.StringVar()
+        self.type_var = tk.StringVar(value="Income")
 
         self.build_form()
         self.build_table()
+        self.refresh_tree()
 
     def build_form(self):
         form = ttk.Frame(self.root, padding=8)
@@ -31,10 +32,11 @@ class ExpenseTrackerApp:
         ttk.Entry(form, width=112, textvariable=self.date_var).grid(row=2, column=1, columnspan=2, padx=10, pady=2, sticky="ew")
 
         ttk.Label(form, text="Type:").grid(row=3, column=0, sticky="e")
-        ttk.Radiobutton(form, text="Income", textvariable=self.type_var, value="Income").grid(row=3, column=1, padx=10, pady=2, sticky="ew")
-        ttk.Radiobutton(form, text="Expense", textvariable=self.type_var, value="Expense").grid(row=3, column=2, padx=10, pady=2, sticky="ew")
+        ttk.Radiobutton(form, text="Income", variable=self.type_var, value="Income").grid(row=3, column=1, padx=10, pady=2, sticky="ew")
+        ttk.Radiobutton(form, text="Expense", variable=self.type_var, value="Expense").grid(row=3, column=2, padx=10, pady=2, sticky="ew")
 
-        # ttk.Button(form, text="Clear", command=self._clear_form).grid(row=4,column=1)
+        ttk.Button(form, text="Clear", command=self.clear_form).grid(row=4, column=2)
+        ttk.Button(form, text="Add", command=self.add_expense).grid(row=4, column=1)
 
     def build_table(self):
         table_frame = ttk.Frame(self.root)
@@ -61,10 +63,33 @@ class ExpenseTrackerApp:
         scroll.pack(side="right", fill="y")
 
         self.table.pack(side="left", fill="both", expand=True)
+
+    def refresh_tree(self):
+        for item in self.table.get_children():
+            self.table.delete(item)
+        
+        all_records = self.record.all()
+        for index, record in enumerate(all_records):
+            self.table.insert("", "end", iid=str(index), values=list(record.values()))
+
+    def clear_form(self):
+        self.name_var.set("")
+        self.amount_var.set("")
+        self.date_var.set("")
+        self.type_var.set("Income")
         
     # Handlers (event handlers)
     def add_expense(self):
-        self.record.add_record("", "", "")
+        name = self.name_var.get()
+        amount = self.amount_var.get()
+        date = self.date_var.get()
+        type_var = self.type_var.get()
+
+        res = self.record.add_record(name, amount, date, type_var)
+        if type(res) == str:
+            raise res
+        
+        self.refresh_tree()
 
     def on_select(self):
         pass
