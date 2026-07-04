a479c8aa008b959667b6e2c316020aaf86e5440d
SeanCrowe1 <sean.crowe2002@gmail.com>
2026-07-04 17:02:56 +0100

    Edited test suite to match updated method names

--- test_expenses.py
+++ test_expenses.py
@@ -7,7 +7,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "1165", "01-06-2026")
         name, amount, date = input_data
-        record.add_record(name, amount, date, "Expense")
+        record.add_expense(name, amount, date, "Expense")
         actual = record.all()
         expected = [{"name": name, "amount": "€1165.00", "date": date, "type": "Expense"}]
         self.assertEqual(actual, expected)
@@ -17,7 +17,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("", "1165", "01-06-2026")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Name is required"
         self.assertEqual(actual, expected)
 
@@ -26,7 +26,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "-1165", "01-06-2026")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Amount must be a valid number greater than 0."
         self.assertEqual(actual, expected)
 
@@ -35,7 +35,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "1165", "01/06/2026")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Date must be valid date in the format 'DD-MM-YYYY'."
         self.assertEqual(actual, expected)
 
@@ -44,7 +44,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "1165", "1-06-2026")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Date must be valid date in the format 'DD-MM-YYYY'."
         self.assertEqual(actual, expected)
 
@@ -53,7 +53,7 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "1165", "01-6-2026")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Date must be valid date in the format 'DD-MM-YYYY'."
         self.assertEqual(actual, expected)
 
@@ -62,15 +62,15 @@ class TestExpenses(unittest.TestCase):
         record.delete_all()
         input_data = ("Rent", "1165", "01-06-26")
         name, amount, date = input_data
-        actual = record.add_record(name, amount, date, "Expense")
+        actual = record.add_expense(name, amount, date, "Expense")
         expected = "Date must be valid date in the format 'DD-MM-YYYY'."
         self.assertEqual(actual, expected)
 
     def test_update_entry(self) -> None:
         record = ExpenseRecord()
         record.delete_all()
-        record.add_record("Rent", "1165", "01-06-2026", "expense")
-        record.update_record(0, "Google", "2115", "25-06-2025", "Income")
+        record.add_expense("Rent", "1165", "01-06-2026", "expense")
+        record.update_expense(0, "Google", "2115", "25-06-2025", "Income")
         actual = record.all()
         expected = [{"name": "Google", "amount": "€2115.00", "date":"25-06-2025", "type":"Income"}]
         self.assertEqual(actual, expected)
@@ -79,7 +79,7 @@ class TestExpenses(unittest.TestCase):
         record = ExpenseRecord()
         record.delete_all()
         id, name, amount, date, type = (0, "Google", "2115", "25-06-2025", "income")
-        actual = record.update_record(id, name, amount, date, type)
+        actual = record.update_expense(id, name, amount, date, type)
         expected = f"Cannot find id in record: {id}"
         self.assertEqual(actual, expected)
 
@@ -87,14 +87,14 @@ class TestExpenses(unittest.TestCase):
         record = ExpenseRecord()
         record.delete_all()
         name, amount, date = ("Rent", "1165", "01-06-2026")
-        record.add_record(name, amount, date, "expense")
-        actual = record.delete_record(0)
+        record.add_expense(name, amount, date, "expense")
+        actual = record.delete_expense(0)
         expected = ""
         self.assertEqual(actual, expected)
 
     def test_invalid_delete(self) -> None:
         record = ExpenseRecord()
         record.delete_all()
-        actual = record.delete_record(0)
+        actual = record.delete_expense(0)
         expected = f"Cannot find id in record: {0}"
         self.assertEqual(actual, expected)
